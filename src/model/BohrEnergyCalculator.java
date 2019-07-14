0d0c1db71d592d7dee9ffcf5fec57a947db74c5e
Dongho Oh <graystone117@gmail.com>
2019-07-14 14:54:09 +0900

    에너지계산기

--- src/model/BohrEnergyCalculator.java
+++ src/model/BohrEnergyCalculator.java
@@ -2,4 +2,17 @@ package model;
 
 public class BohrEnergyCalculator {
 
+    public static double calculate(int start, int end){
+
+        if(start == 0 || end == 0) {
+            return 0;
+        }
+
+        double result = getEnergy(start) - getEnergy(end);
+        return Math.round(result * 10) / 10.0;
+    }
+
+    private static double getEnergy(int floor) {
+        return -1318.0 / Math.pow(floor, 2.0);
+    }
 }
--- src/model/MoleCalculator.java
+++ src/model/MoleCalculator.java
@@ -2,6 +2,8 @@ package model;
 
 import data.Atom;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -24,35 +26,25 @@ public class MoleCalculator {
         System.out.println(calculateMass(""));
     }
 
-    public static int calculateMass(String synthetic) {
+    public static List<String> calculateMass(String synthetic) {
         char[] solute = synthetic.toCharArray();
-        int massResult = 0;
+        List<String> results = new ArrayList<>();
+
         int i = 0;
         while (i < solute.length) {
-            char[] chars = new char[3];
-
-            int count = 0;
-            if(solute[i] >= 'A' && solute[i] <= 'Z') {
-                chars[0] = solute[i];
-                if(solute[i+1] >= 'a' && solute[i+1] <= 'z') {
-                    chars[1] = solute[i+1];
-                    if(solute[i+2] >= '1' && solute[i+2] <= '9') {
-                        Character temp = solute[i+2];
-                        count = Integer.parseInt(temp.toString());
-                        i+=2;
-                    }
-                } else if(solute[i+1] >= '1' && solute[i+1] <= '9') {
-                    Character temp = solute[i+1];
-                    count = Integer.parseInt(temp.toString());
-                } else {
-                    count = 1;
+            int startIndex = 0;
+            if (solute[i] >= 'A' && solute[i] <= 'Z') {
+                startIndex = i;
+                i++;
+                while (!(solute[i] >= 'A' && solute[i] <= 'Z')) {
+                    i++;
                 }
+                results.add(String.copyValueOf(solute).substring(startIndex, i));
+            } else {
+                i++;
             }
-            massResult += Atom.getMassByString(String.copyValueOf(chars).trim()) * count;
-            i++;
-            System.out.println(massResult);
         }
 
-        return massResult;
+        return results;
     }
 }
